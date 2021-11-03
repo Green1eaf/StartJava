9e0cd4431896ea83ae56f3ddab6bb51c862581b7
Green1eaf <89089586+Green1eaf@users.noreply.github.com>
2021-11-03 19:52:34 +0300

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte cores = 6;
+        short ram = 16;
+        int streamS = 12;
+        long freq = 3000;
+        float f = 5.01f;
+        double d = 6.02d;
+        char ch = 'g';
+        boolean bln = true;
+        System.out.println(cores);
+        Sytem.out.println(ram);
+        System.out.println(streamS);
+        System.out.println(freq);
+        
+    }
+}
