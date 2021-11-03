ea0d4e939a02b375df198b4b588eb05f28f16c03
Green1eaf <olegsmirnov86@icloud.com>
2021-11-03 21:25:49 +0300

    corrected

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -2,19 +2,19 @@ public class Variable {
     public static void main(String[] args) {
         byte cores = 6;
         short ram = 16;
-        int streamS = 12;
-        long freq = 3000;
-        float f = 5.01f;
-        double d = 6.02d;
-        char ch = 'g';
-        boolean bln = true;
+        int numberOfStreamsCPU = 12;
+        long freqCPU = 3000;
+        float ssd = 512f;
+        double diagonalOfMonitor = 13.1d;
+        char FirstLiteraOfOS = 'W';
+        boolean haveMouse = true;
         System.out.println(cores);
         System.out.println(ram);
-        System.out.println(streamS);
-        System.out.println(freq);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(ch);
-        System.out.println(bln);
+        System.out.println(numberOfStreamsCPU);
+        System.out.println(freqCPU);
+        System.out.println(ssd);
+        System.out.println(diagonalOfMonitor);
+        System.out.println(FirstLiteraOfOS);
+        System.out.println(haveMouse);
     }
 }
