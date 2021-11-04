08289b0ba5c6f524589c7e75d9f8ec87067d3120
Green1eaf <olegsmirnov86@icloud.com>
2021-11-04 19:49:25 +0300

    1.0

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,31 +1,30 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-        int age = 21;
-        boolean isMale = true;
-        double height = 1.80;
-        char firstLiteraOfName = 'I';
 
-        if(age > 20) {
+        int age = 21;
+        if (age > 20) {
             System.out.println("Age more than 20");
         }
 
-        if(isMale) {
+        boolean isMale = true;
+        if (isMale) {
             System.out.println("Sex is male");
         }
-
-        if(!isMale) {
+        if (!isMale) {
             System.out.println("Sex is not male");
         }
 
-        if(height < 1.80) {
+        double height = 1.80;
+        if (height < 1.80) {
             System.out.println("Height is less than 1.80");
         } else {
             System.out.println("Height is not less than 1.80");
         }
 
-        if(firstLiteraOfName == 'M') {
+        char firstLiteraOfName = 'I';
+        if (firstLiteraOfName == 'M') {
             System.out.println("First litera of name is \"M\"");
-        } else if(firstLiteraOfName == 'I') {
+        } else if (firstLiteraOfName == 'I') {
             System.out.println("First litera of name is \"I\"");
         } else {
             System.out.println("I don't know what is the first litera of name");
