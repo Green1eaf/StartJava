f8ba38d5dc7cf685934f328a9b5609209de4f6ea
Green1eaf <olegsmirnov86@icloud.com>
2021-11-04 21:55:00 +0300

    corrected names of variables

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,25 +1,24 @@
 public class Cycle {
     public static void main(String[] args) {
-
         for (int i = 0; i <= 20; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
 
-        int index = 6;
-        while (index >= -6) {
-            System.out.print(index + " ");
-            index -= 2;
+        int counter = 6;
+        while (counter >= -6) {
+            System.out.print(counter + " ");
+            counter -= 2;
         }
 
-        int sum = 0;
-        int num = 10;
+        int sumOdd = 0;
+        counter = 10;
         do {
-            if (num % 2 != 0) {
-                sum += num;
+            if (counter % 2 != 0) {
+                sumOdd += counter;
             }
-            num++;
-        } while (num <= 20);
-        System.out.println("\n" + sum);
+            counter++;
+        } while (counter <= 20);
+        System.out.println("\n" + sumOdd);
     }
 }
