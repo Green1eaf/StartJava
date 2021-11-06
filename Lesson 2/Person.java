2178b8d3a83abb2e676c45b4a1012a51311a31e2
Green1eaf <olegsmirnov86@icloud.com>
2021-11-06 20:28:27 +0300

    Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    String sex = "Male";
+    String name = "Alex";
+    int height = 186;
+    int weight = 84;
+    int age = 30;
+
+    public void Go() {
+    }
+
+    public void Sit() {
+    }
+
+    public void Run() {
+    }
+
+    public void Speak() {
+    }
+
+    public void LearnJava() {
+    }
+}
