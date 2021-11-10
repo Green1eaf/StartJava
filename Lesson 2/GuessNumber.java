b482f89a0246025635fe2b6ea4f07ec3b87a9fc7
Green1eaf <olegsmirnov86@icloud.com>
2021-11-10 17:41:06 +0300

    cp1251

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class GuessNumber {
     
     public static void main(String[] args) {
-        System.out.println("РЈРіР°РґР°Р№ С‡РёСЃР»Рѕ!");
+        System.out.println("Угадай число!");
         int targetNumber = (int) (Math.random() * 101);
-        Scanner scan = new Scanner(System.in, "Cp866");
-        System.out.print("Р’РІРµРґРёС‚Рµ РёРјСЏ РїРµСЂРІРѕРіРѕ РёРіСЂРѕРєР°: ");
+        Scanner scan = new Scanner(System.in, "Cp1251");
+        System.out.print("Введите имя первого игрока: ");
         Player firstPlayer = new Player(scan.nextLine());
-        System.out.print("Р’РІРµРґРёС‚Рµ РёРјСЏ РІС‚РѕСЂРѕРіРѕ РёРіСЂРѕРєР°: ");
+        System.out.print("Введите имя второго игрока: ");
         Player secondPlayer = new Player(scan.nextLine());
         int answer = 0;
 
@@ -16,33 +16,33 @@ public class GuessNumber {
         while(!repeat.equals("no")) {
             repeat = "";
             while(true) {
-                System.out.print(firstPlayer.getName() + " РІРІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: ");
+                System.out.print(firstPlayer.getName() + " введите число: ");
                 answer = scan.nextInt();
                 scan.nextLine();
                 if (targetNumber < answer) {
-                    System.out.println("Р”Р°РЅРЅРѕРµ С‡РёСЃР»Рѕ Р±РѕР»СЊС€Рµ С‚РѕРіРѕ, С‡С‚Рѕ Р·Р°РіР°РґР°Р» РєРѕРјРїСЊСЋС‚РµСЂ");
+                    System.out.println("Данное число больше того, что загадал компьютер");
                 } else if (targetNumber > answer) {
-                    System.out.println("Р”Р°РЅРЅРѕРµ С‡РёСЃР»Рѕ РјРµРЅСЊС€Рµ С‚РѕРіРѕ, С‡С‚Рѕ Р·Р°РіР°РґР°Р» РєРѕРјРїСЊСЋС‚РµСЂ");
+                    System.out.println("Данное число меньше того, что загадал компьютер");
                 } else if (targetNumber == answer) {
-                    System.out.println("РџРѕР·РґСЂР°РІР»СЏСЋ! РёРіСЂРѕРє " + firstPlayer.getName() + " СѓРіР°РґР°Р» С‡РёСЃР»Рѕ!");
+                    System.out.println("Поздравляю! Игрок " + firstPlayer.getName() + " угадал число!");
                     break;
                 }
 
-                System.out.print(secondPlayer.getName() + " РІРІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: ");
+                System.out.print(secondPlayer.getName() + " введите число: ");
                 answer = scan.nextInt();
                 scan.nextLine();
                 if (targetNumber < answer) {
-                    System.out.println("Р”Р°РЅРЅРѕРµ С‡РёСЃР»Рѕ Р±РѕР»СЊС€Рµ С‚РѕРіРѕ, С‡С‚Рѕ Р·Р°РіР°РґР°Р» РєРѕРјРїСЊСЋС‚РµСЂ");
+                    System.out.println("Данное число больше того, что загадал компьютер");
                 } else if (targetNumber > answer) {
-                    System.out.println("Р”Р°РЅРЅРѕРµ С‡РёСЃР»Рѕ РјРµРЅСЊС€Рµ С‚РѕРіРѕ, С‡С‚Рѕ Р·Р°РіР°РґР°Р» РєРѕРјРїСЊСЋС‚РµСЂ");
+                    System.out.println("Данное число меньше того, что загадал компьютер");
                 } else if (targetNumber == answer) {
-                    System.out.println("РџРѕР·РґСЂР°РІР»СЏСЋ! " + secondPlayer.getName() + " СѓРіР°РґР°Р» С‡РёСЃР»Рѕ!");
+                    System.out.println("Поздравляю! " + secondPlayer.getName() + " угадал число!");
                     break;
                 }
             }
 
             while(!repeat.equals("no") && !repeat.equals("yes")) {
-                System.out.println("РҐРѕС‚РёС‚Рµ РїСЂРѕРґРѕР»Р¶РёС‚СЊ РёРіСЂСѓ? [yes/no]: ");
+                System.out.println("Хотите продолжить игру? [yes/no]: ");
                 repeat = scan.nextLine();
             }
         }
