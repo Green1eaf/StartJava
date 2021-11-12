b44857b99ff6f20e4f9e52497dc5cf29736e28af
Green1eaf <olegsmirnov86@icloud.com>
2021-11-12 20:43:32 +0300

    NoSuchElementException

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -3,40 +3,34 @@ import java.util.Scanner;
 public class GuessNumber {
     private Player firstPlayer;
     private Player secondPlayer;
-    private int targetNumber;
-    private Scanner scan;
 
-    public GuessNumber() {
-        System.out.println("Угадай число!");
-        scan = new Scanner(System.in);
-        System.out.print("Введите имя первого игрока: ");
-        firstPlayer = new Player(scan.nextLine());
-        System.out.print("Введите имя второго игрока: ");
-        secondPlayer = new Player(scan.nextLine());
-    }
-
-    public void setTargetNumber() {
-        targetNumber = (int) (Math.random() * 101);
+    public GuessNumber(Player firstPlayer, Player secondPlayer) {
+        this.firstPlayer = firstPlayer;
+        this.secondPlayer = secondPlayer;
     }
 
     public void play() {
+        int targetNumber = (int) (Math.random() * 101);
+        Scanner scan = new Scanner(System.in);
         while(true) {
             System.out.print(firstPlayer.getName() + " введите число: ");
             int number = scan.nextInt();
-            if(compareNumbers(number)) {
+            if(compareNumbers(number, targetNumber)) {
                 System.out.println("Поздравляю! " + firstPlayer.getName() + " угадал число!");
+                scan.close();
                 break;
             }
             System.out.print(secondPlayer.getName() + " введите число: ");
             number = scan.nextInt();
-            if(compareNumbers(number)) {
+            if(compareNumbers(number, targetNumber)) {
                 System.out.println("Поздравляю! " + secondPlayer.getName() + " угадал число!");
+                scan.close();
                 break;
             }
         }
     }
 
-    private boolean compareNumbers(int number) {
+    private boolean compareNumbers(int number, int targetNumber) {
         if (targetNumber < number) {
             System.out.println("Данное число больше того, что загадал компьютер");
         } else if (targetNumber > number) {
@@ -45,16 +39,5 @@ public class GuessNumber {
             return true;
         }
         return false;
-    }
-
-    public boolean repeatGame() {
-        scan.nextLine();
-        String repeat = "";
-        while(!repeat.equals("no") && !repeat.equals("yes")) {
-            System.out.println("Хотите продолжить игру? [yes/no]: ");
-            repeat = scan.nextLine();
-        }
-        if(repeat.equals("no")) scan.close();
-        return repeat.equals("yes")? true : false;
     }
 }
--- Lesson 2/GuessNumberTest.java
+++ Lesson 2/GuessNumberTest.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class GuessNumberTest {    
     public static void main(String[] args) {
-        GuessNumber game = new GuessNumber();
-        do {
-            game.setTargetNumber();
+        System.out.println("Угадай число!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите имя первого игрока: ");
+        String firstPlayerName = scan.nextLine();
+        Player firstPlayer = new Player(firstPlayerName);
+        System.out.print("Введите имя второго игрока: ");
+        String secondPlayerName = scan.nextLine();
+        Player secondPlayer = new Player(secondPlayerName);
+
+        GuessNumber game = new GuessNumber(firstPlayer, secondPlayer);
+        String repeat = "";
+        while(!repeat.equals("no")) {
+            repeat = "";
             game.play();
-        } while(game.repeatGame());
+            while(!repeat.equals("no") && !repeat.equals("yes")) {
+                System.out.println("Хотите продолжить игру? [yes/no]: ");
+                repeat = scan.nextLine();
+            }
+        }
+        scan.close();
     }
 }
