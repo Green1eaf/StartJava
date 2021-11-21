3d109e08bbb96353a2395c3c5644ccdc1f22a849
green1eaf <aleckid77@gmail.com>
2021-11-21 17:51:54 +0300

    calculator new version is done

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,18 +6,22 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String answer;
         do {
             System.out.print("Введите  математическое выражение (например, 2 * 10): ");
             String expression = scan.nextLine();
             Calculator calculator = new Calculator(expression);
             System.out.println("Результат: " + calculator.calculate());
-
-            do {
-                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                answer = scan.nextLine();
-            } while(!answer.equals("yes") && !answer.equals("no"));
-        } while (answer.equals("yes"));
+        } while (countAgain());
         scan.close();
+    }
+
+    public static boolean countAgain() {
+        String answer;
+        do {
+            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+            Scanner scan = new Scanner(System.in);
+            answer = scan.nextLine();
+        } while(!answer.equals("yes") && !answer.equals("no"));
+        return answer.equals("yes");
     }
 }
