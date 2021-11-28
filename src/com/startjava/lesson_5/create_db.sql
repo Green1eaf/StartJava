ded65d419c8e6073581b7feb566b878ca27180d1
green1eaf <aleckid77@gmail.com>
2021-11-28 20:30:20 +0300

    lesson_5 is done

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE jaegers;
+\c jaegers;
+CREATE TABLE Jaegers (
+id SERIAL PRIMARY KEY,
+modelName TEXT,
+mark TEXT,
+height INTEGER,
+weight INTEGER,
+status TEXT,
+origin TEXT,
+launch INTEGER,
+kaijuKill INTEGER);
---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO Jaegers(modelName,         mark,                     height, weight, status,      origin,   launch, kaijuKill)
+VALUES             ('Crimson Typhoon', 'Mark - 4',               '250',  '1722', 'Destroyed', 'China',  '2018', '5'      ),
+                   ('Bracer Phoenix',  'Mark - 5',               '232',  '2128', 'Destroyed', 'China',  '2025', '9'      ),
+                   ('Cherno Alpha',    'Mark - 1',               '280',  '2412', 'Destroyed', 'Russia', '2015', '14'     ),
+                   ('Maradeur Zeus',   'Unknown classification', '265',  '2021', 'Destroyed', 'USA',    '2031', '7'      ),
+                   ('Coyote Tango',    'Mark - 1',               '280',  '2312', 'Destroyed', 'Japan',  '2015', '4'      ),
+                   ('Murder Witch',    'Mark - 6',               '274',  '1952', 'Ready',     'Russia', '2041', '18'     ),
+                   ('November Ajax',   'Mark - 6',               '259',  '2251', 'Ready',     'Russia', '2037', '23'     ),
+                   ('Obsidian Fury',   'Drone Jaegers',          '257',  '1829', 'Destroyed', 'Russia', '2021', '0'      ),
+                   ('Gipsy Avenger',   'Mark - 6',               '268',  '2004', 'Ready',     'China',  '2034', '1'      ),
+                   ('Saber Athena',    'Mark - 7',               '252',  '1628', 'Ready',     'USA',    '2033', '1'      );
