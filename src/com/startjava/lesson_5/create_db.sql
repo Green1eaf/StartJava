2b2a950c50aa0c2c25cffe8658a0cfaf1467510c
green1eaf <aleckid77@gmail.com>
2021-11-29 20:28:35 +0300

    create_db and init_db is refactored

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE jaegers;
 \c jaegers;
 CREATE TABLE Jaegers (
-id SERIAL PRIMARY KEY,
-modelName TEXT,
-mark TEXT,
-height INTEGER,
-weight INTEGER,
-status TEXT,
-origin TEXT,
-launch INTEGER,
-kaijuKill INTEGER);
+   id SERIAL PRIMARY KEY,
+   modelName TEXT,
+   mark TEXT,
+   height INTEGER,
+   weight INTEGER,
+   status TEXT,
+   origin TEXT,
+   launch INTEGER,
+   kaijuKill INTEGER
+);
